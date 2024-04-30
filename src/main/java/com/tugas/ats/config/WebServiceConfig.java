0a1a015313e7beeb53dbe284a69815d4953f62a8
Ayala-crea <ayalarifki@gmail.com>
2024-04-30 12:54:31 +0700

    connect database

---
+++ src/main/java/com/tugas/ats/config/WebServiceConfig.java
@@ -0,0 +1,5 @@
+package com.tugas.ats.config;
+
+public class WebServiceConfig {
+    
+}
--- src/main/java/com/tugas/ats/repository/StudentRepository.java
+++ src/main/java/com/tugas/ats/repository/StudentRepository.java
@@ -1,6 +1,13 @@
 package com.tugas.ats.repository;
 
-@Repo
-public class StudentRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tugas.ats.model.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+    List<Student> findAll();
 }
